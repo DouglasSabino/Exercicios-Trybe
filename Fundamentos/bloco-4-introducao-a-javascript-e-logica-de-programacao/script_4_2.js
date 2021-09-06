09d4933807a4a403d6764fc61d3b2b82aa39ced8
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-06 13:56:27 -0300

    termina exercicios 1, 2 e 3

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_2.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_2.js
@@ -1,1 +1,24 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 //Exercicio 1
+for(let num of numbers) {
+    console.log(num);
+}
+
+//Exercicio 2
+let soma = 0;
+for(let num of numbers) {
+    soma = soma + num;
+}
+console.log(soma);
+
+//Exercicio 3
+soma = 0;
+for(let num of numbers) {
+    soma = soma + num;
+}
+
+let media = soma / numbers.length
+console.log(media);
+
+//Exercicio 4
