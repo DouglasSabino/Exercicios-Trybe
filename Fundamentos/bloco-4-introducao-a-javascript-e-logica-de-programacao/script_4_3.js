606a35569f5894d2c24980f14c4de9673f0bc16f
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-08 19:05:12 -0300

    termina exercicio 3

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_3.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_3.js
@@ -2,6 +2,7 @@
 let n = 5;
 let simbolo = "*";
 let linha = '';
+let posicao = n;
 
 for(let i = 0; i < n; i++) {
   for(let x = 0; x < n; x++) {
@@ -16,3 +17,21 @@ for(let i = 0; i < n; i++) {
     linha = linha + simbolo;
     console.log(linha);
   }
+
+//Exercicio 3
+linha = '';
+for(let i = 0; i < n; i++) {
+  for(let x = 0; x <= n; x++) {
+   if(x < posicao) {
+     linha += " ";
+  }else {
+    linha += "*";
+   } 
+  }
+
+  console.log(linha);
+  linha = "";
+  posicao--;
+}
+
+ 
