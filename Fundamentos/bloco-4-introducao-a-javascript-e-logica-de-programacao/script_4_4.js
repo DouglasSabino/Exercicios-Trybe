fd49ca5e1b4fad10effb35d18efc2c309cc18eab
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-09 00:52:23 -0300

    termina exercicios 3,4 e 5 da parte I

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_4.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_4.js
@@ -4,9 +4,35 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
+  let newInfo = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+  };  
+
   //Exercicio 1
   console.log("Bem-vinda, " + info.personagem);
 
   //Exercicio 2
   info['recorrente'] = 'sim';
   console.log(info);
+
+  //Exercicio 3
+  for(let atribute in info){
+      console.log(atribute);
+  }
+
+  //Exercicio 4
+  for(let atribute in info){
+    console.log(info[atribute]);
+}
+
+  //Exercicio 5
+  newInfo['recorrente'] = 'sim';
+  for(let atribute in info,newInfo){
+    if(info[atribute] == newInfo[atribute]) {
+      console.log('Ambos recorrentes');
+      break;  
+      }  
+    console.log(info[atribute] + ' e ' + newInfo[atribute]);
+}
