684deb213809df66d6a00c5a996a75f487f6ddeb
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-09 05:27:00 -0300

    termina exercicio 3 parte II

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_4_2.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_4_2.js
@@ -24,3 +24,18 @@ function retornaIndiceDoMaior(vetor) {
 }
 
 console.log(retornaIndiceDoMaior(vetor));
+
+//Exercicio 3 - Parte II
+vetor = [5,3,25,34,2,39,9];
+let menor = vetor[0];
+function retornaIndiceDoMenor(vetor) {
+  for(let num of vetor) {
+    if(num < menor) {
+      menor = num;
+    }
+  }
+  let indexDoMenor = vetor.indexOf(menor);
+  return "O Menor numero está no index " + indexDoMenor + " vetor";
+}
+
+console.log(retornaIndiceDoMenor(vetor));
