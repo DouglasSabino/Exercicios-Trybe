f853dcf9663063802a8fb1dbd4584bf6b0c81e59
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-09 00:36:07 -0300

    termina exerccicio 1 e 2

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script_4_4.js
@@ -0,0 +1,12 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  //Exercicio 1
+  console.log("Bem-vinda, " + info.personagem);
+
+  //Exercicio 2
+  info['recorrente'] = 'sim';
+  console.log(info);
