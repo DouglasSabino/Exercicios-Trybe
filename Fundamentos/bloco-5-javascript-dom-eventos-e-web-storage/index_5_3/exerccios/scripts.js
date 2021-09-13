09077262c628919306809eb225a198e63f8d4426
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-13 19:18:58 -0300

    termina exercicio 1

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/index_5_3/exerccios/scripts.js
@@ -0,0 +1,47 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+  window.onload = function(){ 
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    let ul = document.getElementById('days')
+
+  function criaCalendario() {
+    for(let i = 0; i < dezDaysList.length; i++) {
+      let criaLi = document.createElement('li')
+      if((dezDaysList[i] == 24) || (dezDaysList[i] == 25) || (dezDaysList[i] == 31)) {
+        criaLi.className = 'day holiday'
+        if(dezDaysList[i] == 25) {
+          criaLi.className = 'day holiday friday'
+        }
+      }else if((dezDaysList[i] == 4) || (dezDaysList[i] == 11) || (dezDaysList[i] == 18)) {
+        criaLi.className = 'day friday'
+      }else{
+        criaLi.className = 'day'
+      }
+      criaLi.innerText = dezDaysList[i]
+      ul.appendChild(criaLi);
+    }
+  }
+
+  criaCalendario();
+
+  
+
+
+    
+  console.log(ul)
+
+  }
