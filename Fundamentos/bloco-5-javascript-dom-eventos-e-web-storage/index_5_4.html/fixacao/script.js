690c93fc97b1d84376574f2f3382a906b2d377cf
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-15 18:41:03 -0300

    termina os exerciicos

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/index_5_4.html/fixacao/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/index_5_4.html/fixacao/script.js
@@ -18,7 +18,6 @@ window.onload = function() {
         let paragraph = document.getElementsByClassName('p')
         let input = document.getElementById('input')
         let btn = document.getElementById('color')
-        console.log(paragraph);
             btn.addEventListener('click', function() {
                 for(let i = 0; i < paragraph.length; i++) {
                 paragraph[i].style.color = input.value
@@ -29,4 +28,54 @@ window.onload = function() {
 
     color()
 
+//-------------------------------------------------------------- REQUISITO 2 ---------------------------------------------------------------------------------    
+
+    function fontSize() {
+        let paragraph = document.getElementsByClassName('p')
+        let input = document.getElementById('input')
+        let btn = document.getElementById('fontSize')
+        btn.addEventListener('click', function() {
+            for(let i = 0; i < paragraph.length; i++) {
+            paragraph[i].style.fontSize = input.value
+            }
+            input.value = ''
+        })
+    }
+
+    fontSize()
+
+
+//-------------------------------------------------------------- REQUISITO 3 ---------------------------------------------------------------------------------        
+
+    function espace() {
+        let paragraph = document.getElementsByClassName('p')
+        let input = document.getElementById('input')
+        let btn = document.getElementById('espace') 
+        btn.addEventListener('click', function() {
+            for(let i = 0; i < paragraph.length; i++) {
+            paragraph[i].style.lineHeight = input.value
+            }
+            input.value = ''
+        })
+    }
+
+    espace();
+
+//-------------------------------------------------------------- REQUISITO 4 ---------------------------------------------------------------------------------        
+
+    function fontFamily() {
+        let paragraph = document.getElementsByClassName('p')
+        let input = document.getElementById('input')
+        let btn = document.getElementById('fontFamily') 
+        btn.addEventListener('click', function() {
+            for(let i = 0; i < paragraph.length; i++) {
+            paragraph[i].style.fontFamily = input.value
+            }
+            input.value = ''
+        })
+   }
+
+   fontFamily();
+
+//-------------------------------------------------------------- REQUISITO 5 ---------------------------------------------------------------------------------           
 }
