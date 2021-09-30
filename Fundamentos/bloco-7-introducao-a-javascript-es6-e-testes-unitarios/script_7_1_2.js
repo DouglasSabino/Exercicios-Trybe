143ad145c2e9373e071723dfd89a8d19f5f5bc4e
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-30 17:53:45 -0300

    segundo requisito parte II

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/script_7_1_2.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/script_7_1_2.js
@@ -8,3 +8,20 @@ const fatorial = (num) => {
 fatorial(5)
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const biggerWord = (phrase) => {
+    phrase = phrase.split(' ')
+    let biggerWord = ""
+    for(let i = 0; i < phrase.length; i++){
+        if(phrase[i].length > biggerWord.length) {
+            biggerWord = phrase[i]
+        }
+    }
+    console.log(`a maior palavra é: ${biggerWord}`);
+}
+
+let phrase = "Antônio foi no banheiro e não sabemos o que aconteceu"
+biggerWord(phrase)
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
