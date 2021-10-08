01034386d795d0e967ea5a9d1fe1be833731a410
Douglas Sabino <douglasbsabino@hotmail.com>
2021-10-08 01:10:54 -0300

    exercicio 2

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-ES6/8_1/script_8_1_2.js
@@ -0,0 +1,10 @@
+//Exercicio 2
+const checkNumber = (myNumber, number) => number === myNumber
+
+const loterryNumber = (myNumber, callback) => {
+    const numb = Math.ceil(Math.random()*4)
+
+    return callback(myNumber, numb) ? 'Luck day, you won!' : 'Try Again!'
+}
+
+console.log(loterryNumber(1,checkNumber));
